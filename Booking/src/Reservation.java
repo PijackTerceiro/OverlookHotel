003e11ed5f4c5900c87775902621559cdcbc0d59
Facundo Pagano <104202523+Khundor@users.noreply.github.com>
2022-05-12 19:53:28 +0200

    Update 5

--- Booking/src/Reservation.java
+++ Booking/src/Reservation.java
@@ -1,4 +1,15 @@
 public class Reservation
 {
+  private Guest guest;
+  private Date checkIn;
+  private Date checkOut;
+  private Room room;
 
+  public Reservation(Guest guest,Date checkIn, Date checkOut, Room room)
+  {
+    this.guest = guest;
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+    this.room = room;
+  }
 }
