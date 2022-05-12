56f16df906c69c508c9c066ec31a5893e439e6bd
santimgh <santimiguelgomez@gmail.com>
2022-05-12 19:30:57 +0200

    Date class (Changes in class diagram Date: delete getDate and replace it by getDay, getMonth and getYear; also add equals() boolean method)

---
+++ Test/src/Date.java
@@ -0,0 +1,53 @@
+public class Date
+{
+  private int day, month, year;
+
+  public Date(int day, int month, int year)
+  {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public void setDay(int day)
+  {
+    this.day = day;
+  }
+
+  public void setMonth(int month)
+  {
+    this.month = month;
+  }
+
+  public void setYear(int year)
+  {
+    this.year = year;
+  }
+
+  public int getDay()
+  {
+    return day;
+  }
+
+  public int getMonth()
+  {
+    return month;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
+
+  public Date copy()
+  {
+    return new Date(day, month, year);
+  }
+
+  public boolean equals(Date obj)
+  {
+    return obj.getDay() == day && obj.getMonth() == month
+        && obj.getYear() == year;
+  }
+
+}
