c508fbac5a4af158ada4231d5d5cf4cfbca3dc15
Facundo Pagano <104202523+Khundor@users.noreply.github.com>
2022-05-12 19:40:53 +0200

    Correction 2

--- Booking/src/Room.java
+++ Booking/src/Room.java
@@ -1,4 +1,15 @@
 public class Room
 {
-  private
+  private int price;
+  private int bedNum;
+  private int roomNum;
+  private boolean isBooked;
+
+  public Room(int price, int bedNum, int roomNum)
+  {
+    this.price = price;
+    this.bedNUm = bedNum;
+    this.roomNum = roomNum;
+    this.isBooked = false;
+  }
 }
