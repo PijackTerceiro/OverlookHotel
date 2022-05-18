1faf99ca04d73dcf68cac136a1f53f6e72565c82
PijackTerceiro <104555087+PijackTerceiro@users.noreply.github.com>
2022-05-18 19:33:51 +0200

    Bookings AddMethod

--- Booking/src/Bookings.java
+++ Booking/src/Bookings.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 public class Bookings
 {
+  //Arraylist Field
+  private ArrayList<Reservation>reservations=new ArrayList<>();
 
+  //Constructor
+  public Bookings() {}
+
+  //addReservation
+  public void addReservations(Reservation reservation)
+  {
+    reservations.add(reservation);
+  }
 }
